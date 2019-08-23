90703559f83b9f2bcd5388694dcd9f87ed084dbf
linuxbytes <6693857+linuxbytes@users.noreply.github.com>
2019-08-23 17:43:58 -0400

    add lambdas

--- 005/names.py
+++ 005/names.py
@@ -6,13 +6,15 @@ NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
 
 def dedup_and_title_case_names(names):
     """Should return a list of names, each name appears only once"""
-    pass
+    return list(set(names))
 
 
 def sort_by_surname_desc(names):
     """Returns names list sorted desc by surname"""
     names = dedup_and_title_case_names(names)
     # ...
+    names.sort(key=lambda x:x.split()[-1])
+    return names
 
 
 def shortest_first_name(names):
@@ -21,3 +23,9 @@ def shortest_first_name(names):
     """
     names = dedup_and_title_case_names(names)
     # ...
+    names.sort(key=lambda x:x.split()[0])
+    return names
+
+print(dedup_and_title_case_names(NAMES))
+print(sort_by_surname_desc(NAMES))
+print(shortest_first_name(NAMES))
