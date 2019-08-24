be5f087396446d63881788cb822835faabc317fc
linuxbytes <6693857+linuxbytes@users.noreply.github.com>
2019-08-24 09:06:37 -0400

    add more project

---
+++ 21/cars.py
@@ -0,0 +1,31 @@
+cars = {
+    'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
+    'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
+    'Nissan': ['Maxima', 'Pulsar', '350Z', 'Navara'],
+    'Honda': ['Civic', 'Accord', 'Odyssey', 'Jazz'],
+    'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
+}
+
+
+def get_all_jeeps(cars=cars):
+    """return a comma  + space (', ') separated string of jeep models
+       (original order)"""
+    pass
+
+
+def get_first_model_each_manufacturer(cars=cars):
+    """return a list of matching models (original ordering)"""
+    pass
+
+
+def get_all_matching_models(cars=cars, grep='trail'):
+    """return a list of all models containing the case insensitive
+       'grep' string which defaults to 'trail' for this exercise,
+       sort the resulting sequence alphabetically"""
+    pass
+
+
+def sort_car_models(cars=cars):
+    """return a copy of the cars dict with the car models (values)
+       sorted alphabetically"""
+    pass
