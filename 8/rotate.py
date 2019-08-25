52ba7207d88e4b4c0b38dacc9791cbd24ecc7b14
linuxbytes <6693857+linuxbytes@users.noreply.github.com>
2019-08-25 10:30:23 -0400

    finish rotate byte

--- 8/rotate.py
+++ 8/rotate.py
@@ -2,4 +2,10 @@ def rotate(string, n):
     """Rotate characters in a string.
        Expects string and n (int) for number of characters to move.
     """
-    pass
+    Lfirst = string[0:n]
+    Lsecond = string[n:]
+    return Lsecond + Lfirst
+
+
+print(rotate("hello", 2))
+print(rotate("hello", -2))
