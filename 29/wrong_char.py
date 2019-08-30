dff399331ad8429ff0a2df201cdb77667690c3ef
linuxbytes <6693857+linuxbytes@users.noreply.github.com>
2019-08-30 19:37:59 -0400

    add basic def

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -1,2 +1,13 @@
 def get_index_different_char(chars):
-    pass
+    list_char = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+    for x in chars:
+        if x in list_char:
+            return f"index {chars.index(x)} ('{x}' is non-alphanumeric among alphanumerics)"
+        else:
+            return f"index {chars.index(x)} ('{x}' is alphanumeric among non-alphanumerics)"
+
+
+items = ['1','n','"','4']
+print(get_index_different_char(items))
+
