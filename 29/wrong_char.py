3a2b07c43bd7d21bcb50ddbb84ae6018054c54b6
linuxbytes <6693857+linuxbytes@users.noreply.github.com>
2019-09-01 17:51:14 -0400

    add final code

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -1,16 +1,13 @@
 import string
 
+alphanumeric_chars = list(string.ascii_letters + string.digits)
+
+
 def get_index_different_char(chars):
-
-    list_char = list(string.ascii_letters + string.digits)
-
-    char_index = [chars.index(i) for i in chars if i not in list_char]
-
-    num_index = [chars.index(i) for i in chars if i in list_char]
-
-    return int(char_index[0]) if len(char_index) == 1 else int(num_index[0])
-
-
-items = ['.', '{', ' ^', '%', 'a']
-
-print(get_index_different_char(items))
+    matches, no_matches = [], []
+    for i, char in enumerate(chars):
+        if str(char).lower() in alphanumeric_chars:
+            matches.append(i)
+        else:
+            no_matches.append(i)
+    return matches[0] if len(matches) == 1 else no_matches[0]
