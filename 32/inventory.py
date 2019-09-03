efd0bfd4449bf5385e6e05956bb93afa1dfe92bb
linuxbytes <6693857+linuxbytes@users.noreply.github.com>
2019-09-02 22:17:59 -0400

    fnish bit

--- 32/inventory.py
+++ 32/inventory.py
@@ -1,7 +1,9 @@
+import copy
+
 items = [{'id': 1, 'name': 'laptop', 'value': 1000},
          {'id': 2, 'name': 'chair', 'value': 300},
          {'id': 3, 'name': 'book', 'value': 20}]
 
 
 def duplicate_items(items):
-    return items[:]
+    return copy.deepcopy(items[:])
