0171bf60e003b143ef577370204b3058cf005c6e
linuxbytes <6693857+linuxbytes@users.noreply.github.com>
2019-09-04 22:05:09 -0400

    finish bite

--- 37/countdown.py
+++ 37/countdown.py
@@ -5,4 +5,10 @@ def countdown_for(start=10):
 
 
 def countdown_recursive(start=10):
-    pass
+    if start == 0:
+        print("time is up")
+    else:
+        print(start)
+        return countdown_recursive(start-1)
+
+print (countdown_recursive())
