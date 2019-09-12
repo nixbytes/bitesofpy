815f60bb66cd44e7dfc6547e1c5879d5b0a65fac
linuxbytes <6693857+linuxbytes@users.noreply.github.com>
2019-09-11 21:59:11 -0400

    clean up code

--- 38/nolan.py
+++ 38/nolan.py
@@ -1,14 +1,14 @@
 import xml.etree.ElementTree as ET
 
 # from OMDB
-xmlstring = '''<?xml version="1.0" encoding="UTF-8"?>
+xmlstring = """<?xml version="1.0" encoding="UTF-8"?>
 <root response="True">
   <movie title="The Prestige" year="2006" rated="PG-13" released="20 Oct 2006" runtime="130 min" genre="Drama, Mystery, Sci-Fi" director="Christopher Nolan" />
   <movie title="The Dark Knight" year="2008" rated="PG-13" released="18 Jul 2008" runtime="152 min" genre="Action, Crime, Drama" director="Christopher Nolan" />
   <movie title="The Dark Knight Rises" year="2012" rated="PG-13" released="20 Jul 2012" runtime="164 min" genre="Action, Thriller" director="Christopher Nolan" />
   <movie title="Dunkirk" year="2017" rated="PG-13" released="21 Jul 2017" runtime="106 min" genre="Action, Drama, History" director="Christopher Nolan" />
   <movie title="Interstellar" year="2014" rated="PG-13" released="07 Nov 2014" runtime="169 min" genre="Adventure, Drama, Sci-Fi" director="Christopher Nolan"/>
-</root>'''  # noqa E501
+</root>"""  # noqa E501
 
 
 def get_tree(x=xmlstring):
@@ -20,17 +20,23 @@ def get_tree(x=xmlstring):
 def get_movies():
     """Call get_tree and retrieve all movie titles, return a list or generator"""
     root = get_tree()
-    movies = [(yield movie.attrib['title']) for movie in root.iter(tag='movie')]
+    movies = [(yield movie.attrib["title"]) for movie in root.iter(tag="movie")]
     return movies
+
+
 def _get_runtime(movie):
-    return int(movie.attrib['runtime'].rstrip(' min'))
+    return int(movie.attrib["runtime"].rstrip(" min"))
+
 
 def get_movie_longest_runtime():
     """Call get_tree again and return the movie with the longest runtime in minutes,
        for latter consider adding a _get_runtime helper"""
     root = get_tree()
-    movies = [(movie.attrib['title'], _get_runtime(movie))
-              for movie in root.iter(tag='movie')]
+    movies = [
+        (movie.attrib["title"], _get_runtime(movie)) for movie in root.iter(tag="movie")
+    ]
     max_movie, max_runtime = max(movies, key=lambda m: m[1])
     return max_movie
+
+
 print(get_movie_longest_runtime())
