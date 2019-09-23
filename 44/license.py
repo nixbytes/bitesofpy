b860535d55fa0bb3f2b4d9e69c8a76082f61e984
linuxbytes <6693857+linuxbytes@users.noreply.github.com>
2019-09-22 20:34:17 -0400

    add def and random

--- 44/license.py
+++ 44/license.py
@@ -1,1 +1,13 @@
-def gen_key(...
+import random
+import string
+
+def gen_key(parts, chars_per_part):
+    random_key  = []
+    chars = string.ascii_uppercase +  string.ascii_lowercase  + string.digits
+    for y in range(chars_per_part):
+        random_key = [random.choice(chars) for x in range(parts)]
+        random_key.append('-')
+    return random_key
+
+
+print(gen_key(3,2))
