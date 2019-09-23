ee267e966eb11da14ffc2de64604721f650c82db
linuxbytes <6693857+linuxbytes@users.noreply.github.com>
2019-09-22 21:03:13 -0400

    add join and

--- 44/license.py
+++ 44/license.py
@@ -2,12 +2,15 @@ import random
 import string
 
 def gen_key(parts, chars_per_part):
+    chars = string.ascii_uppercase + string.digits
+    join = ''.join
+
     random_key  = []
-    chars = string.ascii_uppercase +  string.ascii_lowercase  + string.digits
-    for y in range(chars_per_part):
-        random_key = [random.choice(chars) for x in range(parts)]
-        random_key.append('-')
-    return random_key
+
+    while len(random_key) < parts:
+        part_key = join(random.choices(chars, k = chars_per_part))
+        random_key.append(part_key)
+    return '-'.join( x for x in random_key)
 
 
-print(gen_key(3,2))
+print(gen_key(4,9))
